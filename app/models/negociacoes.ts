784479ba6d536931d1e75e5bb5bc9b595b943b2b
Thiago Mesquita <mesquita.thi@hotmail.com>
2022-03-28 22:19:13 -0300

    Adicionando renderização da view

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,16 +1,17 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    //Array<Negociacao> = Negociacao[]
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
     //Retornando uma lista somente leitura (ReadonlyArray)
-    lista(): ReadonlyArray<Negociacao> {
+    //ReadonlyArray<Negociacao> = readonly Negociacao[]
+    lista(): readonly Negociacao[] {
         return this.negociacoes;
     }
 
 }
-
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,11 +1,13 @@
 export class Negociacoes {
     constructor() {
+        //Array<Negociacao> = Negociacao[]
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
     //Retornando uma lista somente leitura (ReadonlyArray)
+    //ReadonlyArray<Negociacao> = readonly Negociacao[]
     lista() {
         return this.negociacoes;
     }
