d0d5d31b8bea046c4144131b2fad0c03d4fc6106
Thiago Mesquita <mesquita.thi@hotmail.com>
2022-03-29 22:54:59 -0300

    Tratando null

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,11 +1,9 @@
 export class Negociacao {
-    //readonly = depois de criado não pode ser alterado
     constructor(_data, quantidade, valor) {
         this._data = _data;
         this.quantidade = quantidade;
         this.valor = valor;
     }
-    //programação defenciva : data
     get data() {
         const data = new Date(this._data.getTime());
         return data;
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,13 +1,10 @@
 export class Negociacoes {
     constructor() {
-        //Array<Negociacao> = Negociacao[]
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
-    //Retornando uma lista somente leitura (ReadonlyArray)
-    //ReadonlyArray<Negociacao> = readonly Negociacao[]
     lista() {
         return this.negociacoes;
     }
